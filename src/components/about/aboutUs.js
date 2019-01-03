ed63c58ee2628ac362e9d62ce637220995646025
Your Name <mstesney12@gmail.com>
2019-01-03 13:30:24 -0600

    cleaned about us breakpoints

--- src/components/about/aboutUs.js
+++ src/components/about/aboutUs.js
@@ -6,8 +6,11 @@ class AboutUs extends Component{
     render(){
         return(
             <div className="about-container">
+            <div className="new">
         <div className="second-text">
-     <p>Big Oaks Self-Storage is not only proud to be family & locally-owned and operated, but also we
+        <h1>OUR STORY</h1>
+        <div className="bar2"></div>
+     <p data-aos="fade-left">Big Oaks Self-Storage is not only proud to be family & locally-owned and operated, but also we
 take pride in the history we bring along with the name “Big Oaks”. When you arrive on site, you can’t
 help but notice several very old oak trees that take residency on our property. When developing the
 property, we preserved these beautiful Oak trees, because of their captivating structure and meaning
@@ -15,9 +18,7 @@ they bring to the Big Oaks Self Storage name. Some of these trees have been stan
 100 years.</p>
 </div>
  <div className="about">
-        <div data-aos="fade-up" className="about-us">
-        <h1>OUR STORY</h1>
-        <div className="bar2"></div>
+        <div data-aos="fade-up" className="third-text">
            <p>Prior to Big Oaks Self Storage, the property would host an annual craft fair know to the locals as
 Quail Oaks Craft Fair. The grounds also served as an RV Park or camp year round. People enjoyed the
 tranquil setting and the shaded areas provided by the giant oak trees. When choosing our units we
@@ -25,7 +26,8 @@ would just like our customers to remember how much pride we not only take in our
 love for our community and land in the “Natural State.”</p>
         </div>
         <div className="storage-div"> 
-        <img src={ StorageContainer} alt="NWA Storage Unit" className="storage-photo" />
+        <img src={ StorageContainer} alt="NWA Storage Unit" className="storage-photo3" />
+        </div>
         </div>
         </div>
             </div>
